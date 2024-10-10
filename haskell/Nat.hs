9295f41b7fa4b1f73a21c251e680cb365842f434
Breno Almeida <brenojalmir9@gmail.com>
2024-10-10 18:54:12 -0300

    Definição de (-), (>), (<) em Nat

--- haskell/Nat.hs
+++ haskell/Nat.hs
@@ -1,6 +1,6 @@
 module Nat where
 
-import Prelude hiding (Num(..), (^), pred, min, max)
+import Prelude hiding (Num(..), (^), pred, min, (<), max, (>), div, quot, rem)
 
 data Nat where
   O :: Nat
@@ -16,6 +16,10 @@ ssso = S sso
 (+) :: Nat -> Nat -> Nat
 n + O = n
 n + S m = S (n + m)
+
+(-) :: Nat -> Nat -> Nat
+n - O = n
+n - S m = pred (n - m)
 
 (*) :: Nat -> Nat -> Nat
 _ * O = O
@@ -44,7 +48,40 @@ min (_, O) = O
 min (O, _) = O
 min (S n, S m) = S (min (n, m))
 
+(<) :: Nat -> Nat -> Bool
+O < O = False
+_ < O = False
+O < n = True
+S n < S m = n < m
+
 max :: (Nat, Nat) -> Nat
 max (n, O) = n
 max (O, n) = n
 max (S n, S m) = S (max (n, m))
+
+(>) :: Nat -> Nat -> Bool
+O > O = False
+O > _ = False
+_ > O = True
+S n > S m = n > m
+
+-- isZero :: Nat -> Bool
+-- isZero O = True
+-- isZero (S _) = False
+
+-- div :: (Nat, Nat) -> (Nat, Nat)
+-- div (O, _) = (O, O)
+-- div (_, O) = error "Division by zero isn't defined"
+-- div (n, S O) = (n, O)
+-- div (n, S (S m)) = (O, S (S m))
+
+
+-- quot :: (Nat, Nat) -> Nat
+-- quot (n, O) = error "Division by zero isn't defined"
+-- quot (n, S O) = n
+-- quot (n, S (S m)) = 
+
+-- rem :: (Nat, Nat) -> Nat
+-- rem (_, O) = error "Division by zero isn't defined"
+-- rem (n, S O) = O
+-- rem (n, S (S m)) = 
