52c8d2e4275ee30f959ebf98f24fba27fc318be4
Breno Almeida <brenojalmir9@gmail.com>
2024-10-09 22:17:49 -0300

    Definição de fib em Nat

--- haskell/Nat.hs
+++ haskell/Nat.hs
@@ -1,10 +1,10 @@
 module Nat where
 
-import Prelude hiding (Num(..), (^), pred)
+import Prelude hiding (Num(..), (^), pred, min, max)
 
 data Nat where
-    O :: Nat
-    S :: Nat -> Nat
+  O :: Nat
+  S :: Nat -> Nat
   deriving (Eq, Show)
 
 o, so, sso, ssso :: Nat
@@ -33,4 +33,16 @@ pred (S n) = n
 
 fact :: Nat -> Nat
 fact O = S O
-fact (S n) = (S n) * fact n
+fact (S n) = S n * fact n
+
+fib :: Nat -> Nat
+fib (S (S x)) = fib (S x) + fib x
+fib _ = S O
+
+-- min :: (Nat, Nat) -> Nat
+-- min (n, O) = O
+-- min (n, S m) = min (pred n, m)
+
+-- max :: (Nat, Nat) -> Nat
+-- max (n, O) = n
+-- max (n, S m) = max (pred n, m)
