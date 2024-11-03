9cc055cd4e25e0b16fca95d2afc021d019e2c1b5
Breno Almeida <brenojalmir9@gmail.com>
2024-11-03 18:04:05 -0300

    Implementation of ArEx

--- haskell/ArEx.hs
+++ haskell/ArEx.hs
@@ -1,7 +1,29 @@
 module ArEx where
 
 data ArEx where
-  (:+) :: Int -> Int -> ArEx
-  (:*) :: Int -> Int -> ArEx
-  (:-) :: Int -> ArEx
-  (:->) :: ArEx -> [ArEx] -> [ArEx]
+  Plus :: ArEx -> ArEx -> ArEx
+  Times :: ArEx -> ArEx -> ArEx
+  Neg :: ArEx -> ArEx
+  Atom :: Integer -> ArEx
+  deriving (Eq, Show)
+
+-- example expressions
+ex1 = (Atom 23) `Plus` (Atom 2)
+ex2 = (Atom 7) `Times` ((Atom 7) `Plus` ((Atom 2) `Times` (Atom 8)))
+ex3 = Times ex1 ex2
+ex4 = Neg $ ex3 `Plus` ex1
+ex5 = (Neg ex1) `Times` (Neg ex4)
+
+-- pretty printer
+pretty :: ArEx -> String
+pretty (Plus exp1 exp2) = "(" ++ pretty exp1 ++ " + " ++ pretty exp2 ++ ")"
+pretty (Times exp1 exp2) = "(" ++ pretty exp1 ++ " * " ++ pretty exp2 ++ ")"
+pretty (Neg exp) = " (-" ++ pretty exp ++ ")"
+pretty (Atom n) =  show n
+
+-- eval evaluates an expression and returns its value
+eval :: ArEx -> Integer
+eval (Plus exp1 exp2) = eval exp1 + eval exp2
+eval (Times exp1 exp2) = eval exp1 * eval exp2
+eval (Neg exp) = - eval exp
+eval (Atom n) = n
--- haskell/hw/ExArith.hs
+++ haskell/hw/ExArith.hs
@@ -6,20 +6,19 @@ data ArEx = Atom Integer
           | Neg ArEx
   deriving (Eq, Show)
 
--- pretty printer
-pretty :: ArEx -> String
-pretty (Plus exp1 exp2) = "(" ++ pretty exp1 ++ " + " ++ pretty exp2 ++ ")"
-pretty (Times exp1 exp2) = "(" ++ pretty exp1 ++ " * " ++ pretty exp2 ++ ")"
-pretty (Neg exp) = " (-" ++ pretty exp ++ ")"
-pretty (Atom n) =  show n
-
-
 -- example expressions
 ex1 = (Atom 23) `Plus` (Atom 2)
 ex2 = (Atom 7) `Times` ((Atom 7) `Plus` ((Atom 2) `Times` (Atom 8)))
 ex3 = Times ex1 ex2
 ex4 = Neg $ ex3 `Plus` ex1
 ex5 = (Neg ex1) `Times` (Neg ex4)
+
+-- pretty printer
+pretty :: ArEx -> String
+pretty (Plus exp1 exp2) = "(" ++ pretty exp1 ++ " + " ++ pretty exp2 ++ ")"
+pretty (Times exp1 exp2) = "(" ++ pretty exp1 ++ " * " ++ pretty exp2 ++ ")"
+pretty (Neg exp) = " (-" ++ pretty exp ++ ")"
+pretty (Atom n) =  show n
 
 -- eval evaluates an expression and returns its value
 eval :: ArEx -> Integer
